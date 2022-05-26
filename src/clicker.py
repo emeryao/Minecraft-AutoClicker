b27946c049a2ecbba46b683cb8cb7a187481312f
Emeryao <emeryao@outlook.com>
2022-05-26 20:21:47 +0800

    feat: exit gracefully on ctrl c

--- src/clicker.py
+++ src/clicker.py
@@ -1,29 +1,44 @@
 import keyboard
 import mouse
 import pyttsx3
+import signal
+import sys
+
+def good_bye():
+    if(mouse.is_pressed(button='right')):
+        mouse.release(button='right')
+    if(mouse.is_pressed(button='left')):
+        mouse.release(button='left')
+
+    speak('good bye')
+    sys.exit()
+
+def signal_handling(signum,frame):
+    print('exit on signal')
+    good_bye()
+
+signal.signal(signal.SIGINT,signal_handling)
 
 def speak(text):
+    print(text)
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
     engine.stop()
-    print(text)
 
 speak('welcome and press esc to exit')
 
 def toggle_button(btn):
     if(mouse.is_pressed(button=btn)):
         mouse.release(button=btn)
-        speak(btn+" released")
+        speak(btn+' released')
     else:
         mouse.press(button=btn)
-        speak("holding "+btn)
+        speak('holding '+btn)
 
 keyboard.add_hotkey('[', lambda:toggle_button('left'))
 keyboard.add_hotkey(']', lambda:toggle_button('right'))
 
 keyboard.wait('esc')
 
-mouse.release(button='right')
-mouse.release(button='left')
-speak("good bye")
+good_bye()
